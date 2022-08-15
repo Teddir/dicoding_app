3c685d1780fae06ceb2ce592eb41119782ea9852
Teddi_r <ferryaja20000@gmail.com>
2022-08-15 22:18:31 +0700

    update validasi user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
 import 'dart:ui';
 
+import 'package:deen/pages/home.dart';
 import 'package:deen/pages/splashScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:carousel_slider/carousel_slider.dart';
@@ -17,12 +20,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final result = FirebaseAuth.instance.currentUser;
+    final isValidasi = result != null
+        ? FirebaseAuth.instance.currentUser!.uid != ''
+        : result != null;
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         debugShowCheckedModeBanner: false,
-        home: const SplashScreenWidget());
+        // ignore: unnecessary_null_comparison
+        home: isValidasi
+            ? const MyHomeWidget(
+                uid: '',
+              )
+            : const SplashScreenWidget());
   }
 }
